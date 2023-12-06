2c52a63fb88567dcf9b6bfe233358a67aa543d9e
1terry <t.juottawa@gmail.com>
2023-12-06 16:53:40 -0500

    update

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,6 +3,36 @@ import React, { useState } from "react";
 function SearchBar({ placeholder, data }) {
   const [filteredData, setFilteredData] = useState([]);
 
+
+  const handleContractorSearch = async (event) => {
+    event.preventDefault();
+    setMessage(''); // Clear previous messages
+
+    // Simple validation
+    try {
+      const response = await fetch('http://localhost:3001/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: email, userType: userType}),
+      });
+
+      const data = await response.json();
+      if (response.status === 201) {
+        console.log('Search Successful', data);
+        setMessage('Search Successful!'); // Display success message
+        // Redirect or handle success scenario
+      } else {
+        console.error('Sign Up Failed emnail:', email,password);
+        setMessage(data.message || 'Sign Up Failed'); // Display error message from server or default message
+      }
+    } catch (error) {
+      console.error('Error during sign up', error);
+      setMessage('An error occurred during sign up'); // Display error message
+    }
+  };
+
   const handleFilter = (event) => {
     const searchWord = event.target.value;
     const newFilter = data.filter((value) => {
