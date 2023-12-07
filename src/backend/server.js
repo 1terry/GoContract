b3358c62e03524d956ae409ae80aacb856408e5c
buruei <buruei@hotmail.com>
2023-12-07 15:57:46 -0500

    123

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: 'C:/Users/Peyman/Desktop/cs4471/GoContract/.env' });
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
