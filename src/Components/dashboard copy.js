c198cca4e1acae5816dccebba2a71a98f4216f36
1terry <t.juottawa@gmail.com>
2023-12-07 16:19:38 -0500

    aaaaaa

--- src/Components/dashboard copy.js
+++ src/Components/dashboard copy.js
@@ -15,10 +15,10 @@ function Dashboard() {
   const handleSearchClick = async (event) => {
     event.preventDefault();
     setMessage('');
-
+  
     try {
       console.log('Sending:', { contractorName: contractorName });
-
+  
       const response = await fetch('http://localhost:3001/search', {
         method: 'POST',
         headers: {
@@ -26,52 +26,41 @@ function Dashboard() {
         },
         body: JSON.stringify({ contractorName: contractorName }),
       });
-
-      const filterRelevantData = (data) => {
-        const dataArray = Array.isArray(data) ? data : [data];
-
-      
-        const excludedKeys = ['_id', '_rev', 'firstName', 'lastName', 'password'];
-      
-        const filteredData = dataArray.map(user => {
-          if (typeof user === 'object') {
-            for (const key of excludedKeys) {
-              delete user[key];
-            }
-            return user;
-          }
-          return {};
-        });
-      
-        return filteredData;
-      };
-      
-      
-    
+  
+      if (!response.ok) {
+        throw new Error(`Query failed with status: ${response.status}`);
+      }
+  
       const data = await response.json();
-
+  
       console.log(data);
-      // Now to format the data
-      // Also send response status to update the query
-
-      const filteredData = filterRelevantData(data);
-      console.log('filtered data!: ', filteredData);
-      setJsonData(JSON.stringify(data.users['username'], null, 2)); // Convert JSON to string with indentation
-
-      if (response.status === 201) {
-        console.log('Query Successful', data);
-        setMessage('Query Successful!');
-        // Redirect or handle success scenario
-        setShowContractorCard(true);
+  
+      // Check if 'users' is an array in the response
+      if (Array.isArray(data.users)) {
+        // Extract usernames from the array
+        const usernames = data.users.map(user => user.username);
+  
+        // Update jsonData with the array of usernames
+        setJsonData(JSON.stringify(usernames, null, 2));
+  
+        if (response.status === 201) {
+          console.log('Query Successful', data);
+          setMessage('Query Successful!');
+          setShowContractorCard(true);
+        } else {
+          console.error('Query Failed:', contractorName);
+          setMessage(data.message || 'Query Failed');
+        }
       } else {
-        console.error('Query Failed:', contractorName);
-        setMessage(data.message || 'Query Failed');
+        console.error('Invalid response format. Expected an array of users.');
+        setMessage('Invalid response format');
       }
     } catch (error) {
       console.error('Error during Query', error);
       setMessage('An error occurred during Query');
     }
   };
+  
 
   return (
     <form onSubmit={handleSearchClick} className='container-search'>
