0a66095be2e536698bd8a43e465143aeca8e24f4
1terry <t.juottawa@gmail.com>
2023-12-07 16:18:43 -0500

    really shit code, like, really shit

--- src/Components/dashboard copy.js
+++ src/Components/dashboard copy.js
@@ -1,51 +1,99 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import './dashboard.css'; // Import the CSS file
+import { useNavigate } from 'react-router-dom';
+import './dashboard.css';
 import ContractorCard from './contractorCard';
-import SearchBar from './SearchBar'; // Adjust the path accordingly
 
 function Dashboard() {
   const [showContractorCard, setShowContractorCard] = useState(false);
-  const navigate = useNavigate(); // Initialize the navigate function
+  const [contractorName, setContractorName] = useState('');
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
+  const [jsonData, setJsonData] = useState(''); // Added state to store JSON data
 
 
+  const handleSearchClick = async (event) => {
+    event.preventDefault();
+    setMessage('');
 
-  const handleSearchClick = () => {
-    // Perform any search logic here
-   
+    try {
+      console.log('Sending:', { contractorName: contractorName });
+
+      const response = await fetch('http://localhost:3001/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ contractorName: contractorName }),
+      });
+
+      const filterRelevantData = (data) => {
+        const dataArray = Array.isArray(data) ? data : [data];
+
+      
+        const excludedKeys = ['_id', '_rev', 'firstName', 'lastName', 'password'];
+      
+        const filteredData = dataArray.map(user => {
+          if (typeof user === 'object') {
+            for (const key of excludedKeys) {
+              delete user[key];
+            }
+            return user;
+          }
+          return {};
+        });
+      
+        return filteredData;
+      };
+      
+      
     
+      const data = await response.json();
 
-    // Navigate to the contractorCard route
-    setShowContractorCard(true);
+      console.log(data);
+      // Now to format the data
+      // Also send response status to update the query
+
+      const filteredData = filterRelevantData(data);
+      console.log('filtered data!: ', filteredData);
+      setJsonData(JSON.stringify(data.users['username'], null, 2)); // Convert JSON to string with indentation
+
+      if (response.status === 201) {
+        console.log('Query Successful', data);
+        setMessage('Query Successful!');
+        // Redirect or handle success scenario
+        setShowContractorCard(true);
+      } else {
+        console.error('Query Failed:', contractorName);
+        setMessage(data.message || 'Query Failed');
+      }
+    } catch (error) {
+      console.error('Error during Query', error);
+      setMessage('An error occurred during Query');
+    }
   };
 
   return (
-    <div className="container-search">
-      <h1>Looking for a contractor? Search below!</h1>
+    <form onSubmit={handleSearchClick} className='container-search'>
+      <h2>Search</h2>
+      {message && <div>{message}</div>}
+      <label>
+        Search for contractor
+        <input 
+          type="text" 
+          value={contractorName} 
+          onChange={(e) => setContractorName(e.target.value)} 
+        />
+      </label>
+      <button type="submit">Search</button>
 
-
-      {/* <input
-        type="text"
-        placeholder={placeholder}
-        value={searchWord}
-        onChange={handleChange}
-      />
-      <button onClick={handleFilter}>Search</button>
-      {filteredData.map((item) => (
-        <div key={item.id}>{item.contractorName}</div>
-      ))} */}
-
-
-      {/* <div className="Search-part">
-        <input className="format" type="text" placeholder="Search here" />
-        <button onClick={handleSearchClick}>Search for kohei</button>      
-        {/* <SearchBar placeholder="Search for contractors"/> */}
-
-
-      {showContractorCard && <ContractorCard />}
-
-
-    </div>
+      <div>
+        <p>JSON Data:</p>
+        <pre>{jsonData}</pre>
+        {/* <ContractorCard /> */}
+      </div>
+      {/* {showContractorCard && <ContractorCard />} */}
+    </form>
   );
 }
 
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -15,10 +15,10 @@ function Dashboard() {
   const handleSearchClick = async (event) => {
     event.preventDefault();
     setMessage('');
-
+  
     try {
       console.log('Sending:', { contractorName: contractorName });
-
+  
       const response = await fetch('http://localhost:3001/search', {
         method: 'POST',
         headers: {
@@ -26,48 +26,41 @@ function Dashboard() {
         },
         body: JSON.stringify({ contractorName: contractorName }),
       });
-
-      const filterRelevantData = (data) => {
-
-        const relevantKeys = ['username', 'userType'];
-        const filteredData = data.map(user => {
-          const filteredUser = {};
-          relevantKeys.forEach(key => {
-            if (user.hasOwnProperty(key)) {
-              filteredUser[key] = user[key];
-            }
-          });
-          return filteredUser;
-        });
-
-        return filteredData;
-      };
-
-
+  
+      if (!response.ok) {
+        throw new Error(`Query failed with status: ${response.status}`);
+      }
+  
       const data = await response.json();
-
+  
       console.log(data);
-      // Now to format the data
-      // Also send response status to update the query
-
-      const filteredData = filterRelevantData(data);
-      console.log('filtered data: ', filteredData);
-      setJsonData(JSON.stringify(filteredData, null, 2)); // Convert JSON to string with indentation
-
-      if (response.status === 201) {
-        console.log('Query Successful', data);
-        setMessage('Query Successful!');
-        // Redirect or handle success scenario
-        setShowContractorCard(true);
+  
+      // Check if 'users' is an array in the response
+      if (Array.isArray(data.users)) {
+        // Extract usernames from the array
+        const usernames = data.users.map(user => user.username);
+  
+        // Update jsonData with the array of usernames
+        setJsonData(JSON.stringify(usernames, null, 2));
+  
+        if (response.status === 201) {
+          console.log('Query Successful', data);
+          setMessage('Query Successful!');
+          setShowContractorCard(true);
+        } else {
+          console.error('Query Failed:', contractorName);
+          setMessage(data.message || 'Query Failed');
+        }
       } else {
-        console.error('Query Failed:', contractorName);
-        setMessage(data.message || 'Query Failed');
+        console.error('Invalid response format. Expected an array of users.');
+        setMessage('Invalid response format');
       }
     } catch (error) {
       console.error('Error during Query', error);
       setMessage('An error occurred during Query');
     }
   };
+  
 
   return (
     <form onSubmit={handleSearchClick} className='container-search'>
